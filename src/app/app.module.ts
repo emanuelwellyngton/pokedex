1e0bd4b1ac67d45535f3f35ae114e019f958cd67
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2024-02-15 09:33:08 -0300

    config: definição da pag home, rota e componente app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { PokemonComponent } from './pages/pokemon/pokemon.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardComponent,
+    PokemonComponent,
+    ErrorPageComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
