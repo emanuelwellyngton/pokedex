9bef9d96cf69179876241bac2d595ad14611d9b5
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2024-02-19 14:44:45 -0300

    config: rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { PokemonComponent } from './pages/pokemon/pokemon.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent, title: 'Pokedex'}
+  {path: '', component: HomeComponent, title: 'Pokedex'},
+  {path: 'pokemon/:id', component: PokemonComponent},
+  {path: '**', component: PageNotFoundComponent, title: 'Conteúdo não existe'}
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,40 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<router-outlet></router-outlet>`,
-  styles: ['']
+  template: `
+  <header>
+    <img class="simbol" src="../../../assets/Logo.png" alt="Imagem de uma Pokebola">
+    <a href=""><h1 class="logotype">Pokedex</h1></a>
+  </header>
+  <router-outlet></router-outlet>`
+  ,
+  styles: [`
+    header {
+    display: flex;
+    justify-content: center;
+    padding: 15px;
+    align-items: center;
+    gap: 10px;
+  }
+
+  .logotype {
+      color: var(--secundary-txt-color);
+      font-size: 32px;
+      line-height: 37.5px;
+      font-style: italic;
+      font-weight: 900;
+  }
+
+  .simbol {
+      height: 35px;
+  }
+
+  .cards {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      gap: 10px;
+  }
+  `]
 })
 export class AppComponent {
   title = 'pokedex';
