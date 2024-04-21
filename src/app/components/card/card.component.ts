6f2260de7533536fba2ec252c0ead482f1cacb46
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2024-04-21 19:26:42 -0300

    refactor: munando a tipagem de pokemon, atributos e adicionando uma classe na imagem

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,21 +1,20 @@
 import { Component, Input } from '@angular/core';
-import { Pokemon } from 'src/app/models/pokemon';
 
 @Component({
   selector: 'app-card',
   template: `
-  <a [routerLink]="'pokemon/' + pokemon?.id">
-    <div class="title-section"><p class="title">{{pokemon?.name}}</p></div>
+  <a *ngIf="pokemon.status" [routerLink]="'pokemon/' + pokemon.status.id">
+    <div class="title-section"><p class="title">{{pokemon.name}}</p></div>
       <div class="card-body">
         <div class="container">
-          <p *ngFor="let item of pokemon?.types" class="tag">{{item.type.name}}</p>
+          <p *ngFor="let item of pokemon.status.types" class="tag">{{item.type.name}}</p>
         </div>
-      <img [src]="pokemon?.image" alt="">
+      <img class="image" [src]="pokemon.status.sprites.other.dream_world.front_default" alt="">
     </div>
   </a>
   `,
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-  @Input() public pokemon: Pokemon | undefined;
+  @Input() public pokemon: any;
 }
